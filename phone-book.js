4b4a02a2a089f88fcea6df6d428f5766963c5877
Sergey Gogolev <gogoleff@yandex-team.ru>
2015-10-06 15:45:05 +0500

    Файловая структура для задачи

---
+++ phone-book.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var phoneBook; // Здесь вы храните записи как хотите
+
+/*
+   Функция добавления записи в телефонную книгу.
+   На вход может прийти что угодно, будьте осторожны.
+*/
+module.exports.add = function add(name, phone, email) {
+
+    // Ваша невероятная магия здесь
+
+};
+
+/*
+   Функция поиска записи в телефонную книгу.
+   Поиск ведется по всем полям.
+*/
+module.exports.find = function find(query) {
+
+    // Ваша удивительная магия здесь
+
+};
+
+/*
+   Функция удаления записи в телефонной книге.
+*/
+module.exports.remove = function remove(query) {
+
+    // Ваша необьяснимая магия здесь
+
+};
+
+/*
+   Функция импорта записей из файла (задача со звёздочкой!).
+*/
+module.exports.importFromCsv = function importFromCsv(filename) {
+    var data = require('fs').readFileSync(filename, 'utf-8');
+
+    // Ваша чёрная магия:
+    // - Разбираете записи из `data`
+    // - Добавляете каждую запись в книгу
+};
+
+/*
+   Функция вывода всех телефонов в виде ASCII (задача со звёздочкой!).
+*/
+module.exports.showTable = function showTable(filename) {
+
+    // Ваша чёрная магия здесь
+
+};
